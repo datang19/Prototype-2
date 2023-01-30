f562532c68392a8d2cad4b3998c377135759d12d
datang73 <datang@s.sfusd.edu>
2023-01-30 11:51:34 -0800

    :)
    Left Barrier

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine; 
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -10) 
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+ 
+    
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
