4aa2f41124d5a369a05bda4ee52d8bde4f1c76c5
datang73 <datang@s.sfusd.edu>
2023-02-02 12:18:27 -0800

    2.2 Food Fight
    Launch Projectile from player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,5 +30,9 @@ public class PlayerController : MonoBehaviour
     
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            //launch a projectile from player
+        }
     }
 }
